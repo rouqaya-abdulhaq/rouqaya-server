1d9dd9b0a816481d081cef181e57b0e80d8db8c4
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-12 15:40:32 +0300

    basic setup for postBlog end point

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || '8000';
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use(bodyParser.json());
+
 app.get('/', (req,res)=>{
     res.status(200).send("initial setup");
 });
+
+app.post('/postBlog', (req, res)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    const blog = {
+        title : title,
+        content : content
+    }
+    res.status(200).send(blog);
+})
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
