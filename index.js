ddb03635e14091cd58b493d9ed0a54f60cf363ac
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-13 16:42:50 +0300

    basic setup for adding new project

--- index.js
+++ index.js
@@ -22,7 +22,20 @@ app.post('/postBlog', (req, res)=>{
         content : content
     }
     res.status(200).send(blog);
-})
+});
+
+app.post('/addProject',(req,res)=>{
+    const title = req.body.title;
+    const info = req.body.info;
+    const imgUrl = req.body.imgUrl;
+
+    const project = {
+        title : title,
+        info : info,
+        imgUrl : imgUrl
+    }
+    res.status(200).send(project);
+});
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
