020ebb61c41d0e9aa4951ec8b176c6f14e156141
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-07-26 17:11:26 +0300

    edit about end point

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -2,16 +2,28 @@ module.exports = (app, client) =>{
     app.get('/loadAbout',(req,res)=>{
         client.query(`SELECT content FROM about WHERE id = 1`,(err,response)=>{
             if(err){
-                DbErrorHandler(res);
+                DbErrorHandler(res,"could not get about page from DB");
             }else{
                 sendAbouteHandler(res,response);
             }
         });
     });
+
+    app.put('/editAbout',(req,res)=>{
+        const newContent = req.body.data.content;
+        const query = editAboutQuery(newContent);
+        client.query(query,(err,response)=>{
+            if(err){
+                DbErrorHandler(res,"could not edit about page");
+            }else{
+                sendAbouteHandler(res,response)
+            }
+        });
+    });
 }
 
-const DbErrorHandler = (res) =>{
-    res.status(500).send({message : "could not get about page from DB", success : false});
+const DbErrorHandler = (res,msg) =>{
+    res.status(500).send({message : msg, success : false});
 }
 
 const sendAbouteHandler = (res,DBresponse) =>{
@@ -26,4 +38,9 @@ const sendAbouteHandler = (res,DBresponse) =>{
     }else{
         res.status(400).send({message : "the provided id does not match the about page id", success : false}); 
     }
+}
+
+
+const editAboutQuery = (newContent) =>{
+    return `UPDATE about SET content ='${newContent}' WHERE id = 1 RETURNING content`;
 }
