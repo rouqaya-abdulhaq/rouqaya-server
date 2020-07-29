68b4e14b8fda59bbb359ad26918cc9b71f6f3f8c
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-07-29 16:05:35 +0300

    end points for translating about

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -20,6 +20,28 @@ module.exports = (app, client) =>{
             }
         });
     });
+
+    app.get('/loadAboutTranslation',(req,res)=>{
+        client.query(`SELECT content FROM about_translation WHERE id = 1`,(err,response)=>{
+            if(err){
+                DbErrorHandler(res,"could not get about translation from DB");
+            }else{
+                sendAbouteHandler(res,response);
+            }
+        });
+    });
+
+    app.put('/editAboutTranslation',(req,res)=>{
+        const newContent = req.body.data.content;
+        const query = editAboutTranslationQuery(newContent);
+        client.query(query,(err,response)=>{
+            if(err){
+                DbErrorHandler(res,"could not edit about page");
+            }else{
+                sendAbouteHandler(res,response)
+            }
+        });
+    });
 }
 
 const DbErrorHandler = (res,msg) =>{
@@ -43,4 +65,8 @@ const sendAbouteHandler = (res,DBresponse) =>{
 
 const editAboutQuery = (newContent) =>{
     return `UPDATE about SET content ='${newContent}' WHERE id = 1 RETURNING content`;
+}
+
+const editAboutTranslationQuery = (newContent) =>{
+    return `UPDATE about_translation SET content ='${newContent}' WHERE id = 1 RETURNING content`;
 }
