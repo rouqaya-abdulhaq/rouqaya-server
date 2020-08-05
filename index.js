1897fd1c50918cabee420e7c49b7a067d1a4be13
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-08-05 20:55:03 +0300

    setup env variables and DB config

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || '8000';
-
+const {client} = require('./config');
+client.connect();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const pg = require('pg');
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const DATABASE_URL = 'postgres://postgres:patapon2012@127.0.0.1:5432/rouqaya_abdulhaq';
-
-const client = new pg.Client({
-    connectionString : DATABASE_URL
-});
-
-client.connect();
 
 const projectsController = require('./controllers/projectsController');
 const blogsController = require('./controllers/blogsController');
